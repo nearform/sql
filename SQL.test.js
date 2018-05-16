8ef478c868b750b044ed60c4982cb130f6290635
Shogun <shogun@cowtech.it>
2018-05-16 11:01:08 +0200

    chore: Reenabled code commented out by mistake.

--- SQL.test.js
+++ SQL.test.js
@@ -2,104 +2,104 @@ const test = require('tap').test
 
 const SQL = require('./SQL')
 
-// test('SQL helper - build complex query with append', (t) => {
-//   const name = 'Team 5'
-//   const description = 'description'
-//   const teamId = 7
-//   const organizationId = 'WONKA'
+test('SQL helper - build complex query with append', (t) => {
+  const name = 'Team 5'
+  const description = 'description'
+  const teamId = 7
+  const organizationId = 'WONKA'
 
-//   const sql = SQL`UPDATE teams SET name = ${name}, description = ${description} `
-//   sql.append(SQL`WHERE id = ${teamId} AND org_id = ${organizationId}`)
+  const sql = SQL`UPDATE teams SET name = ${name}, description = ${description} `
+  sql.append(SQL`WHERE id = ${teamId} AND org_id = ${organizationId}`)
 
-//   t.equal(sql.text, 'UPDATE teams SET name = $1, description = $2 WHERE id = $3 AND org_id = $4')
-//   t.deepEqual(sql.values, [name, description, teamId, organizationId])
-//   t.end()
-// })
+  t.equal(sql.text, 'UPDATE teams SET name = $1, description = $2 WHERE id = $3 AND org_id = $4')
+  t.deepEqual(sql.values, [name, description, teamId, organizationId])
+  t.end()
+})
 
-// test('SQL helper - multiline', (t) => {
-//   const name = 'Team 5'
-//   const description = 'description'
-//   const teamId = 7
-//   const organizationId = 'WONKA'
+test('SQL helper - multiline', (t) => {
+  const name = 'Team 5'
+  const description = 'description'
+  const teamId = 7
+  const organizationId = 'WONKA'
 
-//   const sql = SQL`
-//     UPDATE teams SET name = ${name}, description = ${description}
-//     WHERE id = ${teamId} AND org_id = ${organizationId}
-//   `
+  const sql = SQL`
+    UPDATE teams SET name = ${name}, description = ${description}
+    WHERE id = ${teamId} AND org_id = ${organizationId}
+  `
 
-//   t.equal(sql.text, 'UPDATE teams SET name = $1, description = $2\nWHERE id = $3 AND org_id = $4')
-//   t.deepEqual(sql.values, [name, description, teamId, organizationId])
-//   t.end()
-// })
+  t.equal(sql.text, 'UPDATE teams SET name = $1, description = $2\nWHERE id = $3 AND org_id = $4')
+  t.deepEqual(sql.values, [name, description, teamId, organizationId])
+  t.end()
+})
 
-// test('SQL helper - build complex query with glue', (t) => {
-//   const name = 'Team 5'
-//   const description = 'description'
-//   const teamId = 7
-//   const organizationId = 'WONKA'
+test('SQL helper - build complex query with glue', (t) => {
+  const name = 'Team 5'
+  const description = 'description'
+  const teamId = 7
+  const organizationId = 'WONKA'
 
-//   const sql = SQL` UPDATE teams SET `
+  const sql = SQL` UPDATE teams SET `
 
-//   const updates = []
-//   updates.push(SQL`name = ${name}`)
-//   updates.push(SQL`description = ${description}`)
+  const updates = []
+  updates.push(SQL`name = ${name}`)
+  updates.push(SQL`description = ${description}`)
 
-//   sql.append(sql.glue(updates, ' , '))
-//   sql.append(SQL`WHERE id = ${teamId} AND org_id = ${organizationId}`)
+  sql.append(sql.glue(updates, ' , '))
+  sql.append(SQL`WHERE id = ${teamId} AND org_id = ${organizationId}`)
 
-//   t.equal(sql.text, 'UPDATE teams SET name = $1 , description = $2 WHERE id = $3 AND org_id = $4')
-//   t.deepEqual(sql.values, [name, description, teamId, organizationId])
-//   t.end()
-// })
+  t.equal(sql.text, 'UPDATE teams SET name = $1 , description = $2 WHERE id = $3 AND org_id = $4')
+  t.deepEqual(sql.values, [name, description, teamId, organizationId])
+  t.end()
+})
 
-// test('SQL helper - build complex query with append and glue', (t) => {
-//   const updates = []
-//   const v1 = 'v1'
-//   const v2 = 'v2'
-//   const v3 = 'v3'
-//   const v4 = 'v4'
-//   const v5 = 'v5'
-//   const v6 = 'v6'
-//   const v7 = 'v7'
+test('SQL helper - build complex query with append and glue', (t) => {
+  const updates = []
+  const v1 = 'v1'
+  const v2 = 'v2'
+  const v3 = 'v3'
+  const v4 = 'v4'
+  const v5 = 'v5'
+  const v6 = 'v6'
+  const v7 = 'v7'
 
-//   const sql = SQL`TEST QUERY glue pieces FROM `
-//   updates.push(SQL`v1 = ${v1}`)
-//   updates.push(SQL`v2 = ${v2}`)
-//   updates.push(SQL`v3 = ${v3}`)
-//   updates.push(SQL`v4 = ${v4}`)
-//   updates.push(SQL`v5 = ${v5}`)
+  const sql = SQL`TEST QUERY glue pieces FROM `
+  updates.push(SQL`v1 = ${v1}`)
+  updates.push(SQL`v2 = ${v2}`)
+  updates.push(SQL`v3 = ${v3}`)
+  updates.push(SQL`v4 = ${v4}`)
+  updates.push(SQL`v5 = ${v5}`)
 
-//   sql.append(sql.glue(updates, ' , '))
-//   sql.append(SQL`WHERE v6 = ${v6} `)
-//   sql.append(SQL`AND v7 = ${v7}`)
+  sql.append(sql.glue(updates, ' , '))
+  sql.append(SQL`WHERE v6 = ${v6} `)
+  sql.append(SQL`AND v7 = ${v7}`)
 
-//   t.equal(sql.text, 'TEST QUERY glue pieces FROM v1 = $1 , v2 = $2 , v3 = $3 , v4 = $4 , v5 = $5 WHERE v6 = $6 AND v7 = $7')
-//   t.deepEqual(sql.values, [v1, v2, v3, v4, v5, v6, v7])
-//   t.end()
-// })
+  t.equal(sql.text, 'TEST QUERY glue pieces FROM v1 = $1 , v2 = $2 , v3 = $3 , v4 = $4 , v5 = $5 WHERE v6 = $6 AND v7 = $7')
+  t.deepEqual(sql.values, [v1, v2, v3, v4, v5, v6, v7])
+  t.end()
+})
 
-// test('SQL helper - build complex query with append', (t) => {
-//   const v1 = 'v1'
-//   const v2 = 'v2'
-//   const v3 = 'v3'
-//   const v4 = 'v4'
-//   const v5 = 'v5'
-//   const v6 = 'v6'
-//   const v7 = 'v7'
+test('SQL helper - build complex query with append', (t) => {
+  const v1 = 'v1'
+  const v2 = 'v2'
+  const v3 = 'v3'
+  const v4 = 'v4'
+  const v5 = 'v5'
+  const v6 = 'v6'
+  const v7 = 'v7'
 
-//   const sql = SQL`TEST QUERY glue pieces FROM `
-//   sql.append(SQL`v1 = ${v1}, `)
-//   sql.append(SQL`v2 = ${v2}, `)
-//   sql.append(SQL`v3 = ${v3}, `)
-//   sql.append(SQL`v4 = ${v4}, `)
-//   sql.append(SQL`v5 = ${v5} `)
-//   sql.append(SQL`WHERE v6 = ${v6} `)
-//   sql.append(SQL`AND v7 = ${v7}`)
+  const sql = SQL`TEST QUERY glue pieces FROM `
+  sql.append(SQL`v1 = ${v1}, `)
+  sql.append(SQL`v2 = ${v2}, `)
+  sql.append(SQL`v3 = ${v3}, `)
+  sql.append(SQL`v4 = ${v4}, `)
+  sql.append(SQL`v5 = ${v5} `)
+  sql.append(SQL`WHERE v6 = ${v6} `)
+  sql.append(SQL`AND v7 = ${v7}`)
 
-//   t.equal(sql.text, 'TEST QUERY glue pieces FROM v1 = $1, v2 = $2, v3 = $3, v4 = $4, v5 = $5 WHERE v6 = $6 AND v7 = $7')
-//   t.deepEqual(sql.values, [v1, v2, v3, v4, v5, v6, v7])
-//   t.end()
-// })
+  t.equal(sql.text, 'TEST QUERY glue pieces FROM v1 = $1, v2 = $2, v3 = $3, v4 = $4, v5 = $5 WHERE v6 = $6 AND v7 = $7')
+  t.deepEqual(sql.values, [v1, v2, v3, v4, v5, v6, v7])
+  t.end()
+})
 
 test('SQL helper - supports raw SQL values', (t) => {
   const v1 = 'v1'
