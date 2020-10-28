c5b50e5c2f650185814c879647b8f446b3730668
Simone Busoli <simone.busoli@gmail.com>
2020-10-28 11:29:35 +0100

    move ci to github actions

--- sqlmap/users.js
+++ sqlmap/users.js
@@ -1,40 +1,27 @@
-const { Client } = require('pg')
 const SQL = require('../SQL')
-const config = require('./config')
 
-module.exports = {
-  name: 'users',
-  version: '1.0.0',
-  register: function (server) {
-    const client = new Client(config)
-    return client.connect().then(() => {
-      server.route({
-        method: 'GET',
-        path: '/users',
-        handler: function (request, h) {
-          const { limit = 10, page = 1 } = request.query
+module.exports = async function (fastify) {
+  fastify.get('/users', async request => {
+    const { limit = 10, page = 1 } = request.query
 
-          return client.query(SQL`SELECT * FROM users LIMIT ${limit} OFFSET ${limit * (page - 1)}`)
-            .then((result) => {
-              const users = result.rows
-              return h.response(users)
-            })
-        }
-      })
+    const { rows: users } = await fastify.pg.query(
+      SQL`SELECT * FROM users LIMIT ${limit} OFFSET ${limit * (page - 1)}`
+    )
 
-      server.route({
-        method: 'POST',
-        path: '/users',
-        handler: function (request, h) {
-          const { username, password, email } = request.payload
+    return users
+  })
 
-          return client.query(SQL`INSERT INTO users (username, email, password) VALUES (${username},${email},${password})`)
-            .then((result) => {
-              if (result.command !== 'INSERT' || result.rowCount !== 1) return new Error('User was not inserted')
-              return h.response().code(201)
-            })
-        }
-      })
-    })
-  }
+  fastify.post('/users', async (request, reply) => {
+    const { username, password, email } = request.body
+
+    const result = await fastify.pg.query(
+      SQL`INSERT INTO users (username, email, password) VALUES (${username},${email},${password})`
+    )
+
+    if (result.command !== 'INSERT' || result.rowCount !== 1) {
+      throw new Error('User was not inserted')
+    }
+
+    return reply.code(201).send()
+  })
 }
