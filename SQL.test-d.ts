125b600b2b86b4d430141961e2285f6a404b1037
matt-clarson <clarson469@googlemail.com>
2023-07-04 14:53:18 +0100

    fix: add `map` to the ts definitions file

--- SQL.test-d.ts
+++ SQL.test-d.ts
@@ -1,5 +1,5 @@
 import SQL from '.'
-import { glue, SqlStatement } from '.'
+import { glue, map, SqlStatement } from '.'
 import { expectType, expectError } from 'tsd'
 
 expectType<SqlStatement>(SQL`SELECT 1`)
@@ -8,6 +8,10 @@ expectType<SqlStatement>(SQL`SELECT `.append(SQL`1`))
 expectType<SQL.SqlStatement>(SQL`SELECT `.append(SQL`1`))
 expectType<SqlStatement>(glue([SQL`SELECT`, SQL`1`], ' '))
 expectType<SQL.SqlStatement>(SQL.glue([SQL`SELECT`, SQL`1`], ' '))
+expectType<SQL.SqlStatement>(SQL.map([1,2,3]))
+expectType<SQL.SqlStatement>(SQL.map([1,2,3], x => x**2))
+expectType<SQL.SqlStatement>(map([1,2,3]))
+expectType<SQL.SqlStatement>(map([1,2,3], x => x**2))
 expectType<string>(SQL`SELECT 1`.debug)
 expectType<string>(SQL`SELECT 1`.sql)
 expectType<string>(SQL`SELECT 1`.text)
