dea7ef562052fe6ac6106a7144ea8d500c973f1d
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2024-11-08 20:12:09 +0100

    chore: tests

--- quoteIdentifier.test.js
+++ quoteIdentifier.test.js
@@ -1,36 +1,37 @@
 'use strict'
 
 const { describe, test } = require('node:test')
+const assert = require('node:assert')
 const quoteIdentifier = require('./quoteIdentifier')
 
 describe('quoteIdentifier', () => {
   describe('pg', () => {
     test('simple', async t => {
-      t.assert.deepStrictEqual(quoteIdentifier('identifier', 'pg'), '"identifier"')
+      assert.deepStrictEqual(quoteIdentifier('identifier', 'pg'), '"identifier"')
     })
 
     test('with quotes', async t => {
-      t.assert.deepStrictEqual(quoteIdentifier('"quotes"', 'pg'), '"""quotes"""')
+      assert.deepStrictEqual(quoteIdentifier('"quotes"', 'pg'), '"""quotes"""')
     })
   })
 
   describe('mysql', () => {
     test('simple', t => {
-      t.assert.deepStrictEqual(quoteIdentifier('identifier', 'mysql'), '`identifier`')
+      assert.deepStrictEqual(quoteIdentifier('identifier', 'mysql'), '`identifier`')
     })
 
     test('with quotes', t => {
-      t.assert.deepStrictEqual(quoteIdentifier('`quotes`', 'mysql'), '```quotes```')
+      assert.deepStrictEqual(quoteIdentifier('`quotes`', 'mysql'), '```quotes```')
     })
   })
 
   describe('without type', () => {
     test('simple', t => {
-      t.assert.deepStrictEqual(quoteIdentifier('identifier'), '"identifier"')
+      assert.deepStrictEqual(quoteIdentifier('identifier'), '"identifier"')
     })
 
     test('with quotes', t => {
-      t.assert.deepStrictEqual(quoteIdentifier('"quotes"'), '"""quotes"""')
+      assert.deepStrictEqual(quoteIdentifier('"quotes"'), '"""quotes"""')
     })
   })
 })
